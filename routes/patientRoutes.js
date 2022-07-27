5b58ac1a83996d022cc2d5d747f872831718dcec
Angelo Guilhermino <angeloguilhermino@gmail.com>
2022-07-27 02:15:46 -0300

    CRUD COMPLETED

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -2,12 +2,14 @@ const router = require('express').Router()
 
 const Patient = require('../models/Patient')
 
+//criação de dados
 router.post('/', async (req, res) => {
 
     const {name, healthInsuranceCardId, address, createdAt} = req.body
   
     if(!name) {
       res.status(422).json({ error: 'O nome é obrigatório'})
+      return
     }
   
     const patient = {
@@ -28,5 +30,94 @@ router.post('/', async (req, res) => {
     }
   
   })
+
+  // Leitura de Dados
+  router.get('/', async (req, res) => {
+
+    try {
+      
+      const patients = await Patient.find()
+
+      res.status(200).json(patients)
+
+    } catch (error) {
+      res.status(500).json({error: error})
+    }
+
+  })
+
+  router.get('/:id', async (req, res) => {
+    
+    const id = req.params.id
+
+    try {
+      
+      const patient = await Patient.findOne({_id: id})
+
+      if(!patient) {
+        res.status(422).json({ message: 'Paciente não encontrado!' })
+        return
+      }
+
+      res.status(200).json(patient)
+    } catch (error) {
+      res.status(500).json({error: error})
+    }
+  })
+
+  // Update - atualização de dados (PATCH)
+  router.patch('/:id', async (req, res) => {
+    
+    const id = req.params.id
+
+    const {name, healthInsuranceCardId, address, createdAt} = req.body
+
+    const patient = {
+      name,
+      healthInsuranceCardId,
+      address,
+      createdAt,
+    }
+
+    try {
+
+      const updatedPatient = await Patient.updateOne({_id: id}, patient)
+
+      if(updatedPatient.matchedCount === 0) {
+        res.status(422).json({ message: 'Paciente não encontrado!' })
+        return
+      }
+
+      res.status(200).json(patient)
+      
+    } catch (error) {
+      res.status(500).json({error: error})
+    }
+
+})
+
+// Deletar dados
+router.delete('/:id', async (req, res) => {
+
+  const id = req.params.id
+
+  const patient = await Patient.findOne({_id: id})
+
+      if(!patient) {
+        res.status(422).json({ message: 'Paciente não encontrado!' })
+        return
+      }
+
+      try {
+
+        await Patient.deleteOne({_id: id})
+
+        res.status(200).json({message: 'Paciente Removido com sucesso!'})
+
+      } catch {
+        res.status(500).json({error: error})
+      }
+
+})
 
   module.exports = router
